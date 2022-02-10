d4ae8ac8327940391b1eb65932133332dddfe040
faizalajiw <faizalajiwibowo7@gmail.com>
2022-02-10 21:59:11 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,34 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bangkit-purple": "#5267DF",
+        "bangkit-red": "#FA5959",
+        "bangkit-blue": "#242A45",
+        "bangkit-grey": "#9194A2",
+        "bangkit-white": "#F7F7F7",
+      },
+    },
+    fontFamily: {
+      Poppins: ["Poppins, sans-serif"],
+    },
+    fontWeight: {
+      normal: 400,
+      medium: 500,
+      bold: 700,
+      'extra-bold': 800,
+      black: 900,
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      }
+    }
   },
   plugins: [],
 }
